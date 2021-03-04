edaededcbf996af91de71dc97476dfe2e598b0a7
Victor Zhuravlev <v.a.zhuravlev@gmail.com>
2021-03-04 20:01:33 +0300

    Dummy lexer

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -1,6 +1,49 @@
 #include <stdio.h>
+#include <unordered_map>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <kLexer.h>
 
-int main()
+using namespace std;
+
+enum TCommand {
+    C_PrintTokens,
+};
+
+static unordered_map<string,TCommand> commandsMap = {
+    { "print", C_PrintTokens },
+};
+
+int main( int argc, char* argv[] )
 {
-    printf( "Hello world\n" );
+    if( argc <= 1 || argc > 3 ) {
+        cerr << "Invalid argument count" << endl;
+        return -1;
+    }
+
+    string commandStr = argv[1];
+    string inputFileName = argv[2];
+    auto cmd = commandsMap.find( commandStr );
+    if( cmd == commandsMap.end() ) {
+        cerr << "Unknown command " << commandStr << endl;
+        return -2;
+    }
+    TCommand command = cmd->second;
+
+    ifstream inputFileStream( inputFileName, ios::ios_base::in );
+    if( !inputFileStream ) {
+        cerr << "Unable to open file " << inputFileName << endl;
+        return -3;
+    }
+
+    string content{ istreambuf_iterator<char>( inputFileStream ), istreambuf_iterator<char>() };
+    yyscan_t lexer;
+    if( yylex_init( &lexer ) ) {
+        cerr << "Lexer initialization failed" << endl;
+        return -4;
+    }
+
+    YY_BUFFER_STATE state = yy_scan_string( content.c_str(), lexer );
+    yylex( lexer );
 }
